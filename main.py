206387ff4b6969e07bd4f679f4e5a9ed9ebf970d
Aitoshas <168238284+Aitoshas@users.noreply.github.com>
2024-05-01 05:21:31 +0600

    add new functions:
    1. The Target always move
    2. Score

--- main.py
+++ main.py
@@ -20,6 +20,14 @@ target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
 color = (random.randint(0,255), random.randint(0,255), random.randint(0, 255))
 
+speed = 1
+direction = random.choice(['up', 'down', 'left', 'right'])
+fps = 60
+frame_count = 0
+change_direction_freq = fps * 7
+score = 0
+font = pygame.font.Font(None, 36)
+
 running = True
 while running:
     screen.fill(color)
@@ -31,6 +39,30 @@ while running:
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+                score += 1
+                print("Score: ", score)
+    text = font.render("Score: " + str(score), True, (255, 255, 255))
+    screen.blit(text, (10, 10))
+
+    if direction == 'up':
+        target_y -= speed
+    elif direction == 'down':
+        target_y += speed
+    elif direction == 'left':
+        target_x -= speed
+    elif direction == 'right':
+        target_x += speed
+
+    frame_count += 1
+    if frame_count >= change_direction_freq:
+        direction = random.choice(['up', 'down', 'left', 'right'])
+        frame_count = 0
+
+    if target_x < 0 or target_x > SCREEN_WIDTH - target_width or target_y < 0 or target_y > SCREEN_HEIGHT - target_height:
+        direction = random.choice(['up', 'down', 'left', 'right'])
+
+
+
     screen.blit(target_img, (target_x,target_y))
     pygame.display.update()
 
